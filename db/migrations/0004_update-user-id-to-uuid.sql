26fdb48d7f473031375a12359d6d0ddcf9ab5b5b
joykara <joykarani1@gmail.com>
2025-10-31 20:29:04 +0300

    chore(drizzle): update user_id values to uuid in schema

---
+++ db/migrations/0004_update-user-id-to-uuid.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "analytics" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "carts" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "custom_orders" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "newsletter_subscriptions" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "notifications" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "orders" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "reviews" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "wishlists" ALTER COLUMN "user_id" SET DATA TYPE uuid;
