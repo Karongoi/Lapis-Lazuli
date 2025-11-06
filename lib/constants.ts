3085ff4bd513ea5bdbdec8fe93d93b859a499866
joykara <joykarani1@gmail.com>
2025-11-06 13:47:10 +0300

    chore: add common clothing sizes and colors used across product variants

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,3 +7,30 @@ export const META_IMAGE = "https://res.cloudinary.com/dadkir6u2/image/upload/v17
 export const FAVICON = "https://res.cloudinary.com/dadkir6u2/image/upload/v1738661548/favicon_kngoce.svg"
 export const LOGO = "https://res.cloudinary.com/dadkir6u2/image/upload/v1738661862/logo_vyhw3u.svg"
 export const PRELOADER = "https://res.cloudinary.com/dadkir6u2/video/upload/v1738668407/Animation_-_1738667843996_mjnfws.webm"
+
+// Common clothing sizes used across product variants
+export const CLOTHING_SIZES = [
+    { value: "XS", label: "Extra Small" },
+    { value: "S", label: "Small" },
+    { value: "M", label: "Medium" },
+    { value: "L", label: "Large" },
+    { value: "XL", label: "Extra Large" },
+    { value: "2XL", label: "2X Large" },
+    { value: "3XL", label: "3X Large" },
+    { value: "4XL", label: "4X Large" },
+    { value: "ONE", label: "One Size" },
+]
+
+// Common colors for variants
+export const COMMON_COLORS = [
+    "Black",
+    "White",
+    "Red",
+    "Navy",
+    "Gray",
+    "Charcoal",
+    "Forest Green",
+    "Royal Blue",
+    "Burgundy",
+    "Olive",
+]
